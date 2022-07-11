417f1b0fd4515880716bdc9a3a7f5d99fbc95ea3
NYXTheShadow <alielahirad8087@gmail.com>
2022-07-12 00:51:59 +0430

    core app added.

--- bookmine/settings/common.py
+++ bookmine/settings/common.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -101,3 +102,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
