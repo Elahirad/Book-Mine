3e39b484bd79ceff8d8ab50f9abf152a290fb2b3
NYXTheShadow <alielahirad8087@gmail.com>
2022-07-12 01:37:02 +0430

    Switching from sqlite3 to postgresql.

--- bookmine/settings/dev.py
+++ bookmine/settings/dev.py
@@ -6,7 +6,11 @@ SECRET_KEY = 'django-insecure-7oeaf8v06tpsd-)st!ifgr)vc(pt_o9jqm#67u-veor$5wtb9b
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookmine',
+        'USER': 'bookmineuser',
+        'PASSWORD': 'My@NewPassword12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
